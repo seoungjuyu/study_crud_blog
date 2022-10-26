a75b0db11606bb80f2632c5b5d308eb95b17a363
seungjuyu <2seungju0@gmail.com>
2022-10-26 11:48:22 +0900

    Feat: url 설정완료
    blog url설정후 설정된url로 연결완료

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+def blog_list(request):
+    return HttpResponse("연결 확인")
